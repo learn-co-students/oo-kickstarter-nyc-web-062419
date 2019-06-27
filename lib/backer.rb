7ce639f2c4aff588da30e03e7715af46fa73622d
IanGrubb <yangrubb@gmail.com>
2019-06-27 10:44:34 -0400

    paired with @adriennem76

---
+++ lib/backer.rb
@@ -0,0 +1,24 @@
+
+
+class Backer
+
+    def initialize (name)
+
+        @name = name
+        @backed_projects =[]
+
+    end
+
+    attr_reader :name
+    attr_accessor :backed_projects
+
+    def back_project(project)
+
+        self.backed_projects << project
+        project.backers << self
+
+    end
+
+
+
+end
