0a86f9bc6263919f14d64fa7b21b188a1dbcf0ce
nicholasstano <nicholasstano@gmail.com>
2019-06-27 10:44:45 -0400

    2.0

--- lib/project.rb
+++ lib/project.rb
@@ -1,1 +1,16 @@
 require 'pry'
+
+class Project
+
+	attr_reader :title, :backers
+
+	def initialize(title)
+		@title = title
+		@backers = []
+	end
+
+	def add_backer(backer)
+		@backers << backer
+	end
+
+end
