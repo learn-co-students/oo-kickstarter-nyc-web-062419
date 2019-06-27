bcc5394a2bfdb35098c22fd3d4a42bd2daf31dc0
Anthony Wan <Anthonywan00@gmail.com>
2019-06-27 11:39:19 -0400

    @KConlon13

---
+++ lib/project.rb
@@ -0,0 +1,13 @@
+class Project
+    attr_reader :title, :backers
+
+    def initialize(title)
+        @title = title
+        @backers = []
+    end
+
+    def add_backer(backer_object)
+       @backers << backer_object
+       backer_object.backed_projects << self
+    end
+end
